9b48b0079eeafc2ef7472607cfd9c43f9e79f905
Breugelmans Robin (s148461) <robin.breugelmans@student.ap.be>
2025-03-19 16:43:25 +0100

    add level

---
+++ button_load_scene.gd
@@ -0,0 +1,10 @@
+extends Button
+
+@export
+var scene_name: String
+
+func _ready():
+	self.pressed.connect(_button_pressed)
+
+func _button_pressed():
+	get_tree().change_scene_to_file("res://%s.tscn" % [scene_name])
--- play_button.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-func _ready():
-	self.pressed.connect(_button_pressed)
-
-func _button_pressed():
-	get_tree().change_scene_to_file("res://level_select.tscn")
