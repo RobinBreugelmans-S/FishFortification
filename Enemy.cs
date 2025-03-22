95777f5b3846cd098cec492c5f75d80d02a0df5a
Breugelmans Robin (s148461) <robin.breugelmans@student.ap.be>
2025-03-22 18:11:50 +0100

    add enemy spawning and fullscreen

--- Enemy.cs
+++ Enemy.cs
@@ -1,28 +1,21 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 public partial class Enemy : Sprite2D
 {
-	public int HP;
-	public float spd = 69f;
-	public List<Vector2> track = new() { new(0, 174), new(120, 160), new(254, 111), new(230, 196), new(730, 120) };
-	public int trackIndex = 1;
+	private int hp;
+	private float spd = 69f;
+	private PathFollow2D track;
+
 	override public void _Ready()
 	{
-		//TODO
-		//track = GetNode<Path2D>(new NodePath("../Map/Path").Curve();
-		Position = track[0];
+		track = GetParent<PathFollow2D>();
 	}
+
 	override public void _PhysicsProcess(double delta)
 	{
-		Vector2 toNext = track[trackIndex] - Position;
-		if (toNext.Length() <= 2)
-		{
-			trackIndex++;
-			toNext = track[trackIndex] - Position;
-		}
-
-		Position += toNext.Normalized() * (float)delta * spd;
+		track.Progress += spd * (float)delta;
 	}
 }
