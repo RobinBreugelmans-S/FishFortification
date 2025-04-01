982b3d8739e39122e2b9600c0f9a15d275cc6b4b
Max Poppe <max.poppe@ugent.be>
2025-04-01 17:59:06 +0200

    add waves and better enemy spawning

--- scripts/level_logic.gd
+++ scripts/level_logic.gd
@@ -4,18 +4,30 @@ var stats: RichTextLabel
 var base_hp: int = 127
 
 var wave = 1
+var has_wave_started = true
 var counter = 0
+var enemies_to_spawn = fib(wave)
 var enemy: Resource = preload("res://enemy.tscn")
 var tower: Resource = preload("res://tower.tscn")
 var shrimp_script: Script = load("res://scripts/tower_shrimp.gd")
 var squid_script: Script = load("res://scripts/tower_squid.gd")
+var enemies_spawned = 0
 
 func _ready():
 	stats = $UI/Stats
 
 func _physics_process(delta: float):
-	if (counter % 80 == 0):
+	if (counter % 80 == 0 and enemies_to_spawn > 0):
+		enemies_to_spawn -= 1;
 		spawn_enemy()
+		has_wave_started = true
+	
+	if len(get_tree().get_nodes_in_group("enemy")) == 0 and has_wave_started:
+		wave += 1
+		enemies_to_spawn = fib(wave)
+		has_wave_started = false
+		print(wave, " ", enemies_to_spawn)
+
 	
 	#stats
 	stats.text = ""
@@ -45,3 +57,15 @@ func place_tower(pos: Vector2, tower_script: Script):
 	new_tower.position = pos
 	new_tower.set_script(tower_script)
 	add_child(new_tower)
+
+func fib(num):
+	var a = 0
+	var b = 1
+	var temp = 0
+	
+	for i in range(num):
+		temp = a
+		a = b
+		b = temp + b
+	
+	return b
